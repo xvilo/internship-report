0efeacc94d21a010b46c3c343d4ed7b192296ef7
Sem Schilder <me@sem.design>
2016-03-30 14:07:11 +0100

    basic index

--- html/index.php
+++ html/index.php
@@ -11,13 +11,22 @@
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="description" content="" />
 
-        <title></title>
+        <title>Stage Verslag | Sem Schilder</title>
 
         <link rel="stylesheet" href="/media/stylesheets/app.css" />
         <script src="/media/javascripts/modernizr.js"></script>
+        <script src="https://use.typekit.net/vzn6zga.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
     </head>
     <body>
-        <p>Salut Sem</p>
+        <!-- small-12 medium-8 large-5 -->
+        <section>
+          <div class="grid">
+            <div class="grid__item small-12 medium-12 large-5">
+              <h1 class="section--title">Titel</h1>
+            </div>
+          </div>
+        </section>
         <script async src="/media/javascripts/svg4everybody.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/media/javascripts/jquery.js"><\/script>')</script>
