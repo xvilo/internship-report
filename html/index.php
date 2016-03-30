4fac00783ef42845c17c7ed3f96bd56af9633e7e
Sem Schilder <me@sem.design>
2016-03-30 23:57:10 +0100

    about

--- html/index.php
+++ html/index.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8" />
 
         <link rel="preconnect" href="//ajax.googleapis.com" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
         <meta http-equiv="cleartype" content="on" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -24,7 +25,7 @@
           <a href="#" id="open-about" class="about-toggle about--link"><img src="/media/images/info.png"></a>
         </div>
           <div class="quick-menu">
-            <a href="#" id="open-menu" class="menu-toggle quick-menu--link">
+            <a href="#" id="open-menu" class="menu-toggle quick-menu--link no-underline">
               <span class="quick-menu--block block__red block__rect">
                 <p><img src="/media/images/menu.png" /></p>
               </span>
@@ -35,7 +36,7 @@
               <span class="quick-menu--block block__dark">
                 <p class="quick-menu--text text__center text--small">Hoofdonderwerp</p>
               </span>
-              <a href="https://sem.design" target="_blank" id="go-portfolio" class="quick-menu--link">
+              <a href="https://sem.design" target="_blank" id="go-portfolio" class="quick-menu--link no-underline">
                 <span class="quick-menu--block block__dark block__rect">
                   <p><img src="/media/images/home.png" /></p>
                 </span>
@@ -50,13 +51,13 @@
                 </div>
               </div>
               <div class="grid__item small-12 medium-10 large-10">
-                <p class="body--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu dui laoreet, venenatis elit eu, rutrum lorem. Nam venenatis laoreet massa, a blandit leo efficitur elementum. Morbi consequat turpis id velit suscipit, vel venenatis quam semper. Sed ipsum libero, semper ac risus at, cursus volutpat sapien. Vestibulum at ex dignissim, lobortis sem eget, porttitor ex. Quisque luctus neque vel auctor vulputate. Etiam molestie semper nulla, eu suscipit purus bibendum quis. Quisque tristique sodales erat, sit amet elementum arcu pulvinar at. Mauris lorem enim, egestas a vestibulum a, lacinia non purus. Nullam interdum id erat cursus viverra.<p>
+                <p class="body--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu dui laoreet, venenatis elit eu, rutrum lorem. Nam venenatis laoreet massa, a blandit leo efficitur elementum. <a href="#">dit is een link</a> suscipit, vel venenatis quam semper. Sed ipsum libero, semper ac risus at, cursus volutpat sapien. Vestibulum at ex dignissim, lobortis sem eget, porttitor ex. Quisque luctus neque vel auctor vulputate. Etiam molestie semper nulla, eu suscipit purus bibendum quis. Quisque tristique sodales erat, sit amet elementum arcu pulvinar at. Mauris lorem enim, egestas a vestibulum a, lacinia non purus. Nullam interdum id erat cursus viverra.<p>
 
                 <p class="body--text">Fusce pharetra diam nunc, id faucibus leo viverra a. Etiam luctus congue leo, sed ultricies leo suscipit nec. Duis non imperdiet felis. Cras tincidunt, ipsum non mattis faucibus, neque urna varius lorem, at euismod risus eros luctus lorem. Curabitur sit amet rhoncus arcu, a vestibulum purus. Nulla ultricies at sem et viverra. Sed commodo nunc eget ligula bibendum, sed blandit mi finibus. Aenean a mauris leo. Aliquam erat volutpat. Ut pretium metus tellus, ut feugiat odio malesuada ac. Nam accumsan lacinia sem. Etiam fringilla dapibus tincidunt. Phasellus sodales nibh non metus malesuada aliquet.<p>
 
-                <p class="body--text">Vivamus tincidunt massa at purus dignissim iaculis. Praesent vitae turpis in sapien sodales ultricies vitae in ipsum. Donec scelerisque libero leo, et suscipit tortor vulputate vitae. Nunc egestas odio a lorem congue dictum. Etiam at cursus enim. Suspendisse iaculis neque vitae lectus ullamcorper, id faucibus quam pulvinar. Nullam ornare est nec ipsum imperdiet, id rutrum risus pretium. Fusce pulvinar iaculis erat, et posuere nulla finibus ac. Etiam vitae tempor ipsum, elementum egestas tortor. Nunc dictum egestas ultricies. Duis gravida lacus dolor, eu luctus augue fringilla et. Nulla faucibus dapibus augue, vitae ultrices lacus sodales a.<p>
+                <p class="body--text">Vivamus tincidunt massa at purus dignissim iaculis. Praesent vitae turpis in sapien sodales ultricies vitae in ipsum. Donec scelerisque libero leo, et suscipit tortor vulputate vitae. Nunc egestas odio a lorem congue dictum. Etiam at cursus enim. Suspendisse iaculis neque vitae lectus ullamcorper, <a href="#">id faucibus quam</a> pulvinar. Nullam ornare est nec ipsum imperdiet, id rutrum risus pretium. Fusce pulvinar iaculis erat, et posuere nulla finibus ac. Etiam vitae tempor ipsum, elementum egestas tortor. Nunc dictum egestas ultricies. Duis gravida lacus dolor, eu luctus augue fringilla et. Nulla faucibus dapibus augue, vitae ultrices lacus sodales a.<p>
 
-                <p class="body--text">Nulla faucibus condimentum dui, quis consectetur ante euismod nec. Sed egestas placerat felis vitae luctus. Morbi consequat est sem, id blandit est suscipit et. Mauris finibus ultricies ex sit amet volutpat. Curabitur a accumsan nibh. Praesent id neque porttitor, fringilla dui vel, faucibus libero. Quisque massa velit, ultrices ac elit vel, scelerisque pharetra lacus. Praesent nec urna cursus, ullamcorper nisl sed, malesuada metus. Aenean feugiat enim nec arcu consequat, id vulputate mi efficitur. Etiam at velit ullamcorper justo aliquet tincidunt. Sed id pellentesque nulla, fermentum molestie erat. Quisque congue at ante vel sollicitudin. Vestibulum ut lectus nunc. Vestibulum ac est ante. Nunc id ipsum sed odio ultricies feugiat at congue dui.<p>
+                <p class="body--text">Nulla faucibus condimentum dui, quis consectetur ante euismod nec. Sed egestas placerat felis vitae luctus. Morbi consequat est sem, id blandit est suscipit et. Mauris finibus ultricies ex sit amet volutpat. Curabitur a accumsan nibh. Praesent id neque porttitor, fringilla dui vel, faucibus libero. Quisque massa velit,ultricesac elit vel, scelerisque pharetra lacus. Praesent nec urna cursus, ullamcorper nisl sed, malesuada metus. Aenean feugiat enim nec arcu consequat, id vulputate mi efficitur. Etiam at velit ullamcorper justo aliquet tincidunt. Sed id pellentesque nulla, fermentum molestie erat. Quisque congue at ante vel sollicitudin. Vestibulum ut lectus nunc. Vestibulum ac est ante. Nunc id ipsum sed odio ultricies feugiat at congue dui.<p>
 
                 <p class="body--text">Suspendisse sagittis orci vitae ligula lacinia iaculis. Praesent volutpat felis elementum, semper arcu in, cursus ipsum. Curabitur sollicitudin justo mauris, luctus aliquet eros aliquet in. In sem nisl, malesuada vitae luctus id, commodo in justo. Suspendisse vulputate est quis molestie dapibus. Curabitur at felis ac elit hendrerit efficitur. Donec finibus non ligula at lacinia. Phasellus at vulputate magna. Vivamus at libero ornare, tempor purus at, consequat purus. Nulla accumsan augue non nulla volutpat, scelerisque hendrerit metus sollicitudin. Phasellus felis sem, auctor eget velit eu, tincidunt consequat urna. Nam laoreet consectetur nibh, sit amet condimentum metus sagittis nec. Aliquam rhoncus massa sit amet est maximus hendrerit. Nulla viverra ultricies justo at interdum. Maecenas tincidunt bibendum justo. Sed massa mi, pharetra non risus et, congue tincidunt felis.<p>
               </div>
@@ -68,7 +69,7 @@
             </div>
             <nav class="menu">
               <div class="quick-menu">
-                <a href="#" id="close-menu" class="menu-toggle quick-menu--link">
+                <a href="#" id="close-menu" class="menu-toggle quick-menu--link no-underline">
                   <span class="quick-menu--block block__red block__rect">
                     <p><img src="/media/images/close-menu.png" /></p>
                   </span>
@@ -79,7 +80,7 @@
                   <span class="quick-menu--block block__dark">
                     <p class="quick-menu--text text__center text--small">Hoofdonderwerp</p>
                   </span>
-                  <a href="https://sem.design" target="_blank" id="go-portfolio" class="quick-menu--link">
+                  <a href="https://sem.design" target="_blank" id="go-portfolio" class="quick-menu--link no-underline">
                     <span class="quick-menu--block block__dark block__rect">
                       <p><img src="/media/images/home.png" /></p>
                     </span>
@@ -118,9 +119,29 @@
             </nav>
           </section>
           <section id="about">
-            <div class="menu--wrap">
-              <h1 class="title--text menu--title">about</h1>
+            <div class="about--wrap">
+              <h1 class="title--text about--title">about</h1>
             </div>
+            <ol class="about--list">
+              <li class="about--item" style="transition-delay:0.25s">
+                <h3>what</h3>
+                <p>Dit is het stage verslag van Sem Schilder. Deze stage is gedaan bij <a href="http://builtbybuffalo.com" target="_blank">Built By Buffalo</a> in <a href="http://builtbybuffalo.com" target="_blank">Brighton</a>, UK. De periode was de eerste helft van 2016. Lees dit verslag als je wilt weten wat ik heb geleerd, wat ik leuk vond en wat beter kon!</p>
+              </li>
+              <li class="about--item" style="transition-delay:0.50s">
+                <h3>who</h3>
+                <img src="/media/images/me.jpg">
+                <h4 class="about--person-name">Sem Schilder</h4>
+                <p class="about--person-title">Media Designer, <a class="no-underline" href="https://sem.design">www</a></p>
+              </li>
+              <li class="about--item" style="transition-delay:0.75s">
+                <h3>share</h3>
+                <ol class="social--list clearfix">
+                  <li><a href="#" class="social--item social--item_fb"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#" class="social--item social--item_tw"><i class="fa fa-twitter"></i></a></li>
+                </ol>
+                <p class="about--copy">&copy; Sem Schilder - <?php echo date('Y') ?></p>
+              </li>
+            </ol>
             <div class="about-widget">
               <a href="#" id="open-about" class="about-toggle about--link"><img src="/media/images/close-grey.png"></a>
             </div>
